b1fc83d67022a98c990d298f215e294b7c2866cf
Noxctis <21101819@usc.edu.ph>
2024-11-13 11:51:41 +0800

    change path.

--- Kwartrack/src/login1/src/login1/Login1.java
+++ Kwartrack/src/login1/src/login1/Login1.java
@@ -15,6 +15,7 @@ public class Login1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        UserDAO userDAO = new UserDAO();
         Logn loginFrame = new Logn();
         loginFrame.setVisible(true);
         loginFrame.pack();
