949b1d490eed955085412556914d6c306375461d
Noxctis <21101819@usc.edu.ph>
2024-11-13 12:07:10 +0800

    correct

--- Kwartrack/src/db/ConnectDB.java
+++ Kwartrack/src/db/ConnectDB.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-class ConnectDb {
+public class ConnectDB {
   public static void main(String[] args) throws ClassNotFoundException {
     Connection con = null;
 
