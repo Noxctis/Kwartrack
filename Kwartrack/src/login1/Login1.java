176b69179c3ddbf16499536d664072919cbdc98f
Noxctis <chrysseansevilla@gmail.com>
2024-11-16 13:08:53 +0800

    copy

--- Kwartrack/src/login1/Login1.java
+++ Kwartrack/src/login1/Login1.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package Login.src.login1;
+package login1;
 
 /**
  *
