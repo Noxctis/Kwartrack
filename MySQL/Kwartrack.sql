bce055cd32d2a9a1bfd379b7d72be987f14ddfde
Noxctis <chrysseansevilla@gmail.com>
2024-11-17 20:25:03 +0800

    updated sql aswell

--- MySQL/Kwartrack.sql
+++ MySQL/Kwartrack.sql
@@ -5,6 +5,8 @@ USE kwartrack;
 -- Users table
 CREATE TABLE users (
     user_id INT AUTO_INCREMENT PRIMARY KEY,
+    firstname VARCHAR(100) NOT NULL,
+    lastname VARCHAR(100) NOT NULL,
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password_hash VARCHAR(255) NOT NULL,
