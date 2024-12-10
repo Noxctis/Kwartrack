a4ba3e5e7ee831a00634f4a4d2da3393681cf5e7
CrispyTubig <sidbordario0612@gmail.com>
2024-12-10 20:19:18 +0800

    Charts and buttons and resized a few things

--- netbeans dashboard/school-dashboard-v2/src/form/BalanceChartPane.java
+++ netbeans dashboard/school-dashboard-v2/src/form/BalanceChartPane.java
@@ -22,10 +22,39 @@ public class BalanceChartPane extends javax.swing.JPanel {
 
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        BalanceLineChart = new chart.LineChart();
+        BalanceChartRemoveBalanceButton = new javax.swing.JButton();
+        BalanceChartAddBalanceButton = new javax.swing.JButton();
+
+        setMaximumSize(new java.awt.Dimension(1100, 700));
+        setPreferredSize(new java.awt.Dimension(1000, 600));
 
         jPanel2.setBackground(new java.awt.Color(221, 224, 228));
+        jPanel2.setForeground(new java.awt.Color(102, 102, 102));
 
-        jLabel1.setText("Balance");
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setText("Your Income Trend");
+
+        BalanceLineChart.setToolTipText("");
+        BalanceLineChart.setFocusTraversalPolicyProvider(true);
+        BalanceLineChart.setFocusable(false);
+        BalanceLineChart.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+
+        BalanceChartRemoveBalanceButton.setBackground(new java.awt.Color(204, 204, 204));
+        BalanceChartRemoveBalanceButton.setText("Remove Balance");
+        BalanceChartRemoveBalanceButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BalanceChartRemoveBalanceButtonActionPerformed(evt);
+            }
+        });
+
+        BalanceChartAddBalanceButton.setBackground(new java.awt.Color(204, 204, 204));
+        BalanceChartAddBalanceButton.setText("Deposit Balance");
+        BalanceChartAddBalanceButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                BalanceChartAddBalanceButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
@@ -34,14 +63,27 @@ public class BalanceChartPane extends javax.swing.JPanel {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addContainerGap(2289, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addComponent(BalanceLineChart, javax.swing.GroupLayout.DEFAULT_SIZE, 880, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BalanceChartAddBalanceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(BalanceChartRemoveBalanceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
                 .addComponent(jLabel1)
-                .addContainerGap(553, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BalanceLineChart, javax.swing.GroupLayout.PREFERRED_SIZE, 535, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(BalanceChartAddBalanceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(BalanceChartRemoveBalanceButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(34, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -50,13 +92,33 @@ public class BalanceChartPane extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void BalanceChartRemoveBalanceButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BalanceChartRemoveBalanceButtonActionPerformed
+        // TODO add your handling code here:
+        RemoveWindow removeWindow = new RemoveWindow(); // Pass the reference to ExpensesPane
+        removeWindow.setVisible(true);
+        removeWindow.pack();
+        removeWindow.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        removeWindow.setLocationRelativeTo(null);
+    }//GEN-LAST:event_BalanceChartRemoveBalanceButtonActionPerformed
+
+    private void BalanceChartAddBalanceButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_BalanceChartAddBalanceButtonActionPerformed
+        //TODO add your handling code here:
+
+        depositBalance depositBalanceWindow = new depositBalance(); // rmoved this
+        depositBalanceWindow.setVisible(true);
+        depositBalanceWindow.pack();
+        depositBalanceWindow.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        depositBalanceWindow.setLocationRelativeTo(null);
+
+    }//GEN-LAST:event_BalanceChartAddBalanceButtonActionPerformed
     
 //    public void loadExpenses() {
 //        // Get user ID from the session
@@ -104,6 +166,9 @@ public class BalanceChartPane extends javax.swing.JPanel {
 //    }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton BalanceChartAddBalanceButton;
+    private javax.swing.JButton BalanceChartRemoveBalanceButton;
+    private chart.LineChart BalanceLineChart;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
--- netbeans dashboard/school-dashboard-v2/src/form/DebtsChartPane.java
+++ netbeans dashboard/school-dashboard-v2/src/form/DebtsChartPane.java
@@ -22,26 +22,67 @@ public class DebtsChartPane extends javax.swing.JPanel {
 
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        BalanceLineChart = new chart.LineChart();
+        DebtsChartAddDebtbutton = new javax.swing.JButton();
+        DebtsChartRemoveExpenseButton = new javax.swing.JButton();
+
+        setMaximumSize(new java.awt.Dimension(1100, 700));
+        setPreferredSize(new java.awt.Dimension(1000, 600));
 
         jPanel2.setBackground(new java.awt.Color(221, 224, 228));
 
-        jLabel1.setText("Debgts");
+        jLabel1.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
+        jLabel1.setText("Your Debts Overview");
+
+        BalanceLineChart.setToolTipText("");
+        BalanceLineChart.setFocusTraversalPolicyProvider(true);
+        BalanceLineChart.setFocusable(false);
+        BalanceLineChart.setFont(new java.awt.Font("Segoe UI Black", 0, 14)); // NOI18N
+
+        DebtsChartAddDebtbutton.setBackground(new java.awt.Color(204, 204, 204));
+        DebtsChartAddDebtbutton.setText("Add Debt");
+        DebtsChartAddDebtbutton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DebtsChartAddDebtbuttonActionPerformed(evt);
+            }
+        });
+
+        DebtsChartRemoveExpenseButton.setBackground(new java.awt.Color(204, 204, 204));
+        DebtsChartRemoveExpenseButton.setText("Remove Debt");
+        DebtsChartRemoveExpenseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                DebtsChartRemoveExpenseButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addContainerGap(2289, Short.MAX_VALUE))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(BalanceLineChart, javax.swing.GroupLayout.PREFERRED_SIZE, 880, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(DebtsChartAddDebtbutton, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(DebtsChartRemoveExpenseButton, javax.swing.GroupLayout.PREFERRED_SIZE, 134, javax.swing.GroupLayout.PREFERRED_SIZE)))))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(jLabel1)
-                .addContainerGap(553, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(BalanceLineChart, javax.swing.GroupLayout.PREFERRED_SIZE, 535, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addComponent(DebtsChartAddDebtbutton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(DebtsChartRemoveExpenseButton, javax.swing.GroupLayout.PREFERRED_SIZE, 40, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
@@ -50,13 +91,31 @@ public class DebtsChartPane extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void DebtsChartAddDebtbuttonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DebtsChartAddDebtbuttonActionPerformed
+        // TODO add your handling code here:
+        addDebtWindow addDebt = new addDebtWindow();
+        addDebt.setVisible(true);
+        addDebt.pack();
+        addDebt.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        addDebt.setLocationRelativeTo(null);
+    }//GEN-LAST:event_DebtsChartAddDebtbuttonActionPerformed
+
+    private void DebtsChartRemoveExpenseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_DebtsChartRemoveExpenseButtonActionPerformed
+        // TODO add your handling code here:
+        RemoveWindow removeWindow = new RemoveWindow(); // Pass the reference to ExpensesPane
+        removeWindow.setVisible(true);
+        removeWindow.pack();
+        removeWindow.setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+        removeWindow.setLocationRelativeTo(null);
+    }//GEN-LAST:event_DebtsChartRemoveExpenseButtonActionPerformed
     
 //    public void loadExpenses() {
 //        // Get user ID from the session
@@ -104,6 +163,9 @@ public class DebtsChartPane extends javax.swing.JPanel {
 //    }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private chart.LineChart BalanceLineChart;
+    private javax.swing.JButton DebtsChartAddDebtbutton;
+    private javax.swing.JButton DebtsChartRemoveExpenseButton;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel2;
     // End of variables declaration//GEN-END:variables
